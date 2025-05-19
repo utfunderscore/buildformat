9d9233f9c41c4187979ebe884420182a13dda5cc
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-05-19 19:12:46 +0100

    chore: Rename build meta store

--- common/src/main/java/org/readutf/buildformat/common/meta/BuildStore.java
+++ common/src/main/java/org/readutf/buildformat/common/meta/BuildStore.java
@@ -1,22 +1,23 @@
 package org.readutf.buildformat.common.meta;
 
 import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.readutf.buildformat.common.exception.BuildFormatException;
 import org.readutf.buildformat.common.format.BuildFormatChecksum;
 
-public interface BuildMetaStore {
+public interface BuildStore {
 
     @NotNull BuildMeta create(String name, String description) throws BuildFormatException;
 
     @Nullable BuildMeta getByName(String name) throws BuildFormatException;
 
-    void setFormats(String name, List<BuildFormatChecksum> formats) throws BuildFormatException;
+    BuildMeta update(String name, List<BuildFormatChecksum> formats) throws BuildFormatException;
 
     @NotNull List<String> getBuilds() throws BuildFormatException;
 
-    @NotNull List<String> getBuildsByFormat(@NotNull String formatName) throws BuildFormatException;
+    @NotNull Map<String, BuildFormatChecksum> getBuildsByFormat(@NotNull String formatName) throws BuildFormatException;
 
 
 }
