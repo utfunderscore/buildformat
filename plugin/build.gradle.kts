0c435440d51004b33c05df7d6e946bd7fc5e60b1
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-06-10 23:02:09 +0100

    feat: Add maven repositories for FAWE

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -19,6 +19,8 @@ java {
 repositories {
     mavenCentral()
     maven { url = uri("https://repo.panda-lang.org/releases") }
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://maven.enginehub.org/repo/")
 }
 
 dependencies {
