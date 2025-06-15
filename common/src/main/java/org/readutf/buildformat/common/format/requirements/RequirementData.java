714898c1d277e8e7a842984229b6af1674e751f2
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-06-15 22:32:12 +0100

    chore: Add Jackson annotations for serialization support and bump version to 1.0.13

--- common/src/main/java/org/readutf/buildformat/common/format/requirements/RequirementData.java
+++ common/src/main/java/org/readutf/buildformat/common/format/requirements/RequirementData.java
@@ -1,5 +1,8 @@
 package org.readutf.buildformat.common.format.requirements;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+
 public record RequirementData(
         RequirementType type,
         int minimumAmount
--- common/src/main/java/org/readutf/buildformat/common/format/requirements/RequirementType.java
+++ common/src/main/java/org/readutf/buildformat/common/format/requirements/RequirementType.java
@@ -1,7 +1,12 @@
 package org.readutf.buildformat.common.format.requirements;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public interface RequirementType {
 
+    @JsonIgnore
     String getRegex();
 
 }
