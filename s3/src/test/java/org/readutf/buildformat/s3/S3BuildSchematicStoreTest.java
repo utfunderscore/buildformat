2d54add07e33f0268170cf44d92e31a655c0de09
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-07-11 15:07:38 +0100

    test: Add test for S3BuildSchematicStore with real S3 configuration and sample data file

--- s3/src/test/java/org/readutf/buildformat/s3/S3BuildSchematicStoreTest.java
+++ s3/src/test/java/org/readutf/buildformat/s3/S3BuildSchematicStoreTest.java
@@ -1,6 +1,5 @@
 package org.readutf.buildformat.s3;
 
-import org.junit.Rule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.readutf.buildformat.common.exception.BuildFormatException;
@@ -11,7 +10,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.CreateBucketResponse;
 
@@ -21,18 +20,18 @@ import java.nio.file.Path;
 
 class S3BuildSchematicStoreTest {
 
-    static DockerImageName localstackImage = DockerImageName.parse("localstack/localstack:3.5.0");
-
-    static LocalStackContainer localstack =
-            new LocalStackContainer(localstackImage).withServices(LocalStackContainer.Service.S3);
-
     @BeforeAll
-    public static void beforeAll() {
-        localstack.start();
-    }
+    public static void beforeAll() {}
 
     @Test
     void testUploadAndRetrieval() throws IOException, BuildFormatException {
+
+        DockerImageName localstackImage = DockerImageName.parse("localstack/localstack:3.5.0");
+
+        LocalStackContainer localstack =
+                new LocalStackContainer(localstackImage).withServices(LocalStackContainer.Service.S3);
+
+        localstack.start();
 
         S3AsyncClient s3 = S3AsyncClient.builder()
                 .endpointOverride(localstack.getEndpoint())
@@ -58,6 +57,36 @@ class S3BuildSchematicStoreTest {
         BuildSchematic downloaded = store.load("Atlantis");
 
         System.out.println(downloaded);
+    }
 
+    @Test
+    void testS3() throws IOException, BuildFormatException {
+
+        String s3AccessKeyId = System.getenv("S3_ACCESS_KEY_ID");
+        String s3SecretAccessKey = System.getenv("S3_SECRET_ACCESS_KEY");
+        String bucket = System.getenv("S3_BUCKET");
+
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(s3AccessKeyId, s3SecretAccessKey);
+
+        S3Configuration serviceConfiguration =
+                S3Configuration.builder().pathStyleAccessEnabled(true).build();
+
+        S3AsyncClient client = S3AsyncClient.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(Region.US_EAST_1)
+                .serviceConfiguration(serviceConfiguration)
+                .build();
+
+        S3BuildSchematicStore store = new S3BuildSchematicStore(client, bucket);
+
+        // Load Atlantis.schem from resources
+        BuildSchematic schematic = new BuildSchematic(
+                "Atlantis", Files.readAllBytes(Path.of("src", "test", "resources", "Atlantis.schem")));
+
+        store.save(schematic);
+
+        BuildSchematic downloaded = store.load("Atlantis");
+
+        System.out.println(downloaded);
     }
 }
