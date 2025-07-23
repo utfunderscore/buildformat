ff46607cad1cb1414577fd271387f27f2e58b399
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-07-23 13:32:21 +0100

    feat: Implement ExposedMetaStore for SQL-based build metadata management and update interfaces for nullability

--- postgres/src/main/resources/db/migration/V2__Create_buildformat.sql
+++
@@ -1,7 +0,0 @@
-create table buildmeta_format(
-    id serial primary key,
-    buildmeta_id int not null,
-    name varchar(16) not null,
-    checksum varchar(64) not null,
-    FOREIGN KEY (buildmeta_id) REFERENCES buildmeta(id)
-)
--- postgres/src/main/resources/db/migration/V3__Create_buildtags_table.sql
+++
@@ -1,8 +0,0 @@
-create table buildmeta_tags
-(
-    id serial primary key,
-    buildmeta_id int not null,
-    tag varchar(32) not null,
-    FOREIGN KEY (buildmeta_id) REFERENCES buildmeta(id)
-);
-
