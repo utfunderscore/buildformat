c6cc9823cbd2d27630c1366886255a98a2051ef4
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-12-10 00:36:01 +0000

    fix: Add correct JsonProperty annotations to IntegerRequirement.java

--- common/src/main/java/org/readutf/buildformat/requirement/types/number/IntegerRequirement.java
+++ common/src/main/java/org/readutf/buildformat/requirement/types/number/IntegerRequirement.java
@@ -1,11 +1,15 @@
 package org.readutf.buildformat.requirement.types.number;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jetbrains.annotations.NotNull;
 import org.readutf.buildformat.requirement.types.NumberRequirement;
 
 public class IntegerRequirement extends NumberRequirement<Integer> {
 
-    public IntegerRequirement(@NotNull String name, Integer min, Integer max) {
+    public IntegerRequirement(
+            @JsonProperty("name") @NotNull String name,
+            @JsonProperty("min") Integer min,
+            @JsonProperty("max") Integer max) {
         super(name, min, max);
     }
 }
